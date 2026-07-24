9b2fcb6b3e1a0a1ec00ecb2b411363e8dc12f923
Eugene Kimathi <eugenekimathi@gmail.com>
2026-07-24 21:40:14 +0300

    Added schema to the models

--- server/schemas.py
+++ server/schemas.py
@@ -1,37 +1,37 @@
-from marshmallow import fields
-from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
+# from marshmallow import fields
+# from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
-from app import db
-from models import *
+# from app import db
+# from models import *
 
-class CustomerSchema(SQLAlchemyAutoSchema):
+# class CustomerSchema(SQLAlchemyAutoSchema):
 
-    class Meta:
-        model = Customer
-        load_instance = True
+#     class Meta:
+#         model = Customer
+#         load_instance = True
 
-    id = fields.Int()
-    name = fields.Str()
-    reviews = fields.Nested("ReviewSchema", many=True, exclude=("customer","item"))
+#     id = fields.Int()
+#     name = fields.Str()
+#     reviews = fields.Nested("ReviewSchema", many=True, exclude=("customer","item"))
 
-class ItemSchema(SQLAlchemyAutoSchema):
+# class ItemSchema(SQLAlchemyAutoSchema):
 
-    class Meta:
-        model = Item
-        load_instance = True
+#     class Meta:
+#         model = Item
+#         load_instance = True
 
-    id = fields.Int()
-    name = fields.Str()
-    price = fields.Float()
-    reviews = fields.Nested("ReviewSchema", many=True, exclude=("item","customer"))
+#     id = fields.Int()
+#     name = fields.Str()
+#     price = fields.Float()
+#     reviews = fields.Nested("ReviewSchema", many=True, exclude=("item","customer"))
 
-class ReviewSchema(SQLAlchemyAutoSchema):
+# class ReviewSchema(SQLAlchemyAutoSchema):
 
-    class Meta:
-        model = Review
-        load_instance = True
+#     class Meta:
+#         model = Review
+#         load_instance = True
 
-    id = fields.Int()
-    comment = fields.Str()
-    customer = fields.Nested("CustomerSchema", exclude=("reviews","items"))
-    item = fields.Nested("ItemSchema", exclude=("reviews",))     
+#     id = fields.Int()
+#     comment = fields.Str()
+#     customer = fields.Nested("CustomerSchema", exclude=("reviews","items"))
+#     item = fields.Nested("ItemSchema", exclude=("reviews",))     
