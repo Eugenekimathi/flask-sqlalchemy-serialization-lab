4a8d96dc5cf05fd78b2d96b4df96c0761a8b5528
Eugene Kimathi <eugenekimathi@gmail.com>
2026-07-24 21:04:14 +0300

    Fixed Schemas error

--- server/schemas.py
+++ server/schemas.py
@@ -1,21 +1,36 @@
-from marshmallow import fields, validate, validates, ValidationError
+from marshmallow import fields
 from marshmallow_sqlalchemy import SQLAlchemyAutoSchema
 
 from app import db
-from app.models import Customer , Item , Review
+from models import Customer , Item , Review
 
 class CustomerSchema(SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Customer
+        load_instance = True
+
     id = fields.Int()
     name = fields.Str()
     reviews = fields.Nested("ReviewSchema", many=True, exclude=("customer","item"))
 
 class ItemSchema(SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Item
+        load_instance = True
+
     id = fields.Int()
     name = fields.Str()
     price = fields.Float()
     reviews = fields.Nested("ReviewSchema", many=True, exclude=("item","customer"))
 
 class ReviewSchema(SQLAlchemyAutoSchema):
+
+    class Meta:
+        model = Review
+        load_instance = True
+        
     id = fields.Int()
     comment = fields.Str()
     customer = fields.Nested("CustomerSchema", exclude=("reviews","items"))
